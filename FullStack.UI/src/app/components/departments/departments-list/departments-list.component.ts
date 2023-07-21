8ef3cbff2c9bbbe7bf8c5b68275004cbb4fd1bac
Olar-Alex-Student <alex.olar@student.usv.ro>
2023-07-21 16:51:39 +0300

    Creat serviciu si modificat lista de departamente

--- FullStack.UI/src/app/components/departments/departments-list/departments-list.component.ts
+++ FullStack.UI/src/app/components/departments/departments-list/departments-list.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Department } from 'src/app/models/department.model';
+import { DepartmentsService } from 'src/app/services/departments.service';
 
 @Component({
   selector: 'app-departments-list',
   templateUrl: './departments-list.component.html',
   styleUrls: ['./departments-list.component.css']
 })
-export class DepartmentsListComponent {
+export class DepartmentsListComponent implements OnInit {
 
+  departments: Department[] = [];
+
+  // ctor
+  constructor(private departmentService: DepartmentsService) {
+  }
+
+  // ngOnInit
+  ngOnInit(): void {
+    this.departmentService.getAllDepartments()
+    .subscribe({
+      next: (departments: any) => {
+        this.departments = departments  
+        console.log(this.departments);
+      },
+      error: (response: any) => {
+        console.log(response);
+      }
+    })
+  }
 }
---
+++ FullStack.UI/src/app/services/departments.service.ts
@@ -0,0 +1,52 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Department } from '../models/department.model';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class DepartmentsService {
+
+  baseApiURL: string = 'https://localhost:7090'
+
+  constructor(private http: HttpClient) { }
+
+  // GET toate Departamentele din baza de date
+  getAllDepartments(): Observable<Department[]> {
+
+    // 
+    return this.http.get<Department[]>(this.baseApiURL + "/api/Departments");
+  }
+
+  // POST un nou departament in baza de date
+  addNewDepartment(addDepartmentRequest: Department): Observable<Department>{
+
+    // Initializare Guid Empty pentru fiecare angajat nou
+    addDepartmentRequest.id = "00000000-0000-0000-0000-000000000000"
+
+    // 
+    return this.http.post<Department>(this.baseApiURL + "/api/Departments", addDepartmentRequest);
+  }
+
+  // GET un singur departament
+  getDepartment(id: string): Observable<Department>{
+
+    // 
+    return this.http.get<Department>(this.baseApiURL + "/api/Departments/" + id)
+  }
+
+  // PUT edit departament
+  updateDepartment(id: string, updateDepartmentRequest: Department ): Observable<Department> {
+
+    // 
+    return this.http.put<Department>(this.baseApiURL + "/api/Departments/" + id, updateDepartmentRequest)
+  }
+
+  // DELTE departament
+  deleteDepartment(id: string): Observable<Department> {
+
+    // 
+    return this.http.delete<Department>(this.baseApiURL + "/api/Departments/" + id)
+  }
+}
